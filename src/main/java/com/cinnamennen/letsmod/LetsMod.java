e3fab2d91f77797c1ca5a51ed2049a029e8b60e4
caleb <calebmennen@gmail.com>
2014-07-02 21:27:48 -0500

    First Commit

---
+++ src/main/java/com/cinnamennen/letsmod/LetsMod.java
@@ -0,0 +1,28 @@
+package com.cinnamennen.letsmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "LetsMod", name = "Lets Mod", version = "1.0")
+public class LetsMod {
+
+    @Mod.Instance("LetsMod")
+    public static LetsMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+
+    }
+}
---
+++ src/main/java/com/cinnamennen/letsmod/proxy/CommonProxy.java
@@ -0,0 +1,7 @@
+package com.cinnamennen.letsmod.proxy;
+
+/**
+ * Created by caleb on 7/2/14.
+ */
+public class CommonProxy implements IProxy {
+}
